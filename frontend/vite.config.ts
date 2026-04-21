cf1000bf80b64b741dac74ab27fa416f83f1d7e3
Jay Gupta <ag8615340@gmail.com>
2026-04-21 14:14:15 +0530

    Vite config and css parser fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/codeboard/",
+  // base: "/codeboard/",  <-- Ise hata do agar aap custom domain/root par ho
   server: {
     port: 5173,
   },
